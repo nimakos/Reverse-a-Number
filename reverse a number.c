9babcc397404a204cb8d5d459b55f4d1f0c9ace0
Vangelis Nikolis <nimakos@hotmail.com>
2018-07-25 00:51:32 +0300

    Update reverse a number.c

--- reverse a number.c
+++ reverse a number.c
@@ -12,22 +12,6 @@ int main()
       reverse = reverse * 10 + n%10;
             n = n/10;
    }
-   /*
-n = 4562
-reverse = 0
-
-reverse = reverse *10 + n%10 = 0 + 2 = 2
-n = n/10 = 456
-
-reverse = reverse *10 + n%10 = 20 + 6 = 26
-n = n/10 = 45
-
-reverse = reverse *10 + n%10 = 260 + 5 = 265
-n = n/10 = 4
-
-reverse = reverse *10 + n%10 = 2650 + 4 = 2654
-n = n/10 = 0
-  */
 
    printf("Reverse of entered nber is = %d\n", reverse);
 
